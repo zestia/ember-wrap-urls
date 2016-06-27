18835a92b430190d3878f06e6249e8a362612f09
Andrew Kirwin <andrew.kirwin@gmail.com>
2016-06-27 13:33:00 +0100

    upgrade ember-cli

--- index.js
+++ index.js
@@ -2,18 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-wrap-urls',
-
-  included: function(app, parentAddon) {
-    var target = (parentAddon || app);
-
-    // target.options = target.options || {};
-    // target.options.babel = target.options.babel || {};
-    // target.options.babel.plugins = target.options.babel.plugins || [];
-    // target.options.babel.plugins.push('transform-es2015-sticky-regex');
-
-    // app.import(app.bowerDirectory + '/unicode-string-utils/unicode-string-utils.js');
-
-    return this._super.included(target);
-  }
+  name: 'ember-wrap-urls'
 };
