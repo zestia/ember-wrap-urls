5027307dd107ab321d1c676359ebc856fa05341e
Andrew Kirwin <andrew.kirwin@zestia.com>
2018-06-08 13:40:04 +0100

    upgrade templates, correct template name for link component

--- addon/components/wrap-urls/link.js
+++ addon/components/wrap-urls/link.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import layout from '../../templates/components/wrap-urls/hyperlink';
+import layout from '../../templates/components/wrap-urls/link';
 
 export default Component.extend({
   layout,
