a79fa3e6f3873ba0e09b34f0eb6bf9922d610457
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-08-20 10:35:21 +0100

    update emoji

--- tests/dummy/app/utils/samples.js
+++ tests/dummy/app/utils/samples.js
@@ -3,7 +3,7 @@ http: http://foo.com
 https: https://bar.com
 ftp: ftp://baz.com
 file: file://qux.jpg
-emoji: 💩 http://norf.com
+emoji: 🔗http://norf.com
 subdomain: http://foo.bar.com
 path: http://foo.com/bar/baz
 query: http://foo.com?bar=baz
