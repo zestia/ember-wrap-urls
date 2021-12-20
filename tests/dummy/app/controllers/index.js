e0a15d2a0eb5406dc8069a19d4e639ace28282cd
Andrew Kirwin <github@andrewkirwin.co.uk>
2021-12-20 17:21:55 +0000

    remove action

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,13 +1,11 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { text } from '../utils/samples';
 import { tracked } from '@glimmer/tracking';
 
 export default class Indexontroller extends Controller {
   @tracked text = text;
 
-  @action
-  setText({ target: { value } }) {
+  setText = ({ target: { value } }) => {
     this.text = value;
-  }
+  };
 }
