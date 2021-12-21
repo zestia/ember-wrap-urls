a914522323a9d26dc8df433b805a405fbe7875f0
Andrew Kirwin <github@andrewkirwin.co.uk>
2021-12-21 19:25:04 +0000

    prefer action decorator

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 import { text } from '../utils/samples';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class Indexontroller extends Controller {
+export default class IndexController extends Controller {
   @tracked text = text;
 
-  setText = ({ target: { value } }) => {
+  @action
+  setText({ target: { value } }) {
     this.text = value;
-  };
+  }
 }
